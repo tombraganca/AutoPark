bd638195cc41ea6d244fcfb4304b5554d42bd86c
Tom Bragança <gleydiston35@gmail.com>
2023-05-06 17:53:51 -0300

    remoção de código sensível

--- server/src/providers/messaging/MessagingProvider.ts
+++ server/src/providers/messaging/MessagingProvider.ts
@@ -19,7 +19,6 @@ interface IPayload {
 }
 export class MessagingProvider {
     async send(payload: IPayload): Promise<string> {
-        payload.token = "cSC5pcvZQ7OkCJMuy-XS0v:APA91bFE4BMjtxPcjkAe_1PTWlhzHmLQE79_RvrBF_tUY8GQQiqQhEL7Dp8CMIdaQ0VFJT5Wc-FHGjwM2WNPaefr1cw_QLSzK5sf9ty-mj399tWHGUpV8oNNu6PuVcYCV1D7N_hhLH4Z"
         return await admin.messaging().send(payload)
     }
 }
