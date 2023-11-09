94f973ea946311e1021b9533c5de3a1a8847e065
Gleydiston Bragança <gleydiston.santos@actdigital.com>
2023-11-09 20:29:27 -0300

    Update Prisma version and delete unnecessary migrations

--- server/prisma/migrations/20230506190840_criacao_do_campo_divice_em_account_token/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `device` to the `account_tokens` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "account_tokens" ADD COLUMN     "device" TEXT NOT NULL;
---
+++ server/prisma/migrations/20231109232830_criacao_da_base_de_dados/migration.sql
@@ -0,0 +1,91 @@
+-- CreateTable
+CREATE TABLE "accounts" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "refresh_tokens" (
+    "id" TEXT NOT NULL,
+    "expiresIn" INTEGER NOT NULL,
+    "accountId" TEXT NOT NULL,
+
+    CONSTRAINT "refresh_tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "cars" (
+    "plate" TEXT NOT NULL,
+    "model" TEXT NOT NULL,
+    "manufacturer" TEXT NOT NULL,
+    "ownerId" TEXT NOT NULL,
+
+    CONSTRAINT "cars_pkey" PRIMARY KEY ("plate")
+);
+
+-- CreateTable
+CREATE TABLE "accesses" (
+    "id" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "plate" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "ownerId" TEXT NOT NULL,
+
+    CONSTRAINT "accesses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "account_tokens" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "device" TEXT NOT NULL,
+    "accountId" TEXT NOT NULL,
+
+    CONSTRAINT "account_tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "vacancies" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "situation" TEXT,
+    "businessId" TEXT NOT NULL,
+    "plateCar" TEXT,
+
+    CONSTRAINT "vacancies_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "business" (
+    "id" TEXT NOT NULL,
+    "business" TEXT NOT NULL,
+
+    CONSTRAINT "business_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "accounts_email_key" ON "accounts"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "vacancies_title_key" ON "vacancies"("title");
+
+-- AddForeignKey
+ALTER TABLE "refresh_tokens" ADD CONSTRAINT "refresh_tokens_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "accounts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "cars" ADD CONSTRAINT "cars_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "accounts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "accesses" ADD CONSTRAINT "accesses_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "accounts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "account_tokens" ADD CONSTRAINT "account_tokens_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "accounts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "vacancies" ADD CONSTRAINT "vacancies_businessId_fkey" FOREIGN KEY ("businessId") REFERENCES "business"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
